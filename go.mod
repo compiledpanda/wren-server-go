f7973ef6ed784ab9e3be3ad971c960f466b48777
John Clark <socrates37@gmail.com>
2021-10-13 06:52:42 -0600

    wip

--- go.mod
+++ go.mod
@@ -2,11 +2,10 @@ module github.com/compiledpanda/wren-server-go
 
 go 1.17
 
-require github.com/gorilla/mux v1.8.0
-
 require (
+	github.com/gorilla/mux v1.8.0
 	github.com/boltdb/bolt v1.3.1
+	github.com/rs/zerolog v1.25.0
+	github.com/golang-jwt/jwt/v4 v4.1.0
 	golang.org/x/sys v0.0.0-20211007075335-d3039528d8ac // indirect
 )
-
-require github.com/rs/zerolog v1.25.0
--- server/auth.go
+++ server/auth.go
@@ -1,6 +1,11 @@
 package server
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+
+	jwt "github.com/golang-jwt/jwt/v4"
+)
 
 func authenticate(r *http.Request, cfg *Config) (userId string, e serverError) {
 	auth := r.Header.Get("Authorization")
@@ -13,6 +18,24 @@ func authenticate(r *http.Request, cfg *Config) (userId string, e serverError) {
 	// Ensure that the value is "Bearer <jwt>"
 
 	// Ensure that the jwt is valid
+
+	tokenString := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmb28iOiJiYXIiLCJuYmYiOjE0NDQ0Nzg0MDB9.u1riaD1rW97opCoAuRCTy4w58Br-Zk-bh7vLiRIsrpU"
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		return []byte("my_secret_key"), nil
+	})
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		fmt.Println(claims["foo"], claims["nbf"])
+	} else {
+		fmt.Println(err)
+	}
 
 	// Validate the JWT
 	return
